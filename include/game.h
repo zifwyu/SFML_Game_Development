cc76af7d72319f72402540da036524dba05f24ad
June <Juneee@88.com>
2026-08-06 21:11:52 +0800

    feat: 增加方向控制功能，但现在移动太快了

--- include/game.h
+++ include/game.h
@@ -15,7 +15,14 @@ private:
     // 绘制和渲染
     void Render();
 
+    void HandlePlayerInput(sf::Keyboard::Scancode key, bool is_pressed);
+
 private:
     sf::RenderWindow window_;
     sf::CircleShape player_;
+
+    bool is_move_up_ = false;
+    bool is_move_down_ = false;
+    bool is_move_left_ = false;
+    bool is_move_right_ = false;
 };
--- src/game.cpp
+++ src/game.cpp
@@ -17,17 +17,61 @@ void Game::Run() {
 }
 
 void Game::ProcessEvents() {
-    while (auto event = window_.pollEvent()) {
-        if (event->is<sf::Event::Closed>()) {
-            window_.close();
-        }
-    }
+    const auto on_close = [this](const sf::Event::Closed&) {
+        window_.close();
+        };
+
+    const auto on_key_pressed = [this](const sf::Event::KeyPressed& key) {
+        HandlePlayerInput(key.scancode, true);
+        };
+
+    const auto on_key_released = [this](const sf::Event::KeyReleased& key) {
+        HandlePlayerInput(key.scancode, false);
+        };
+    
+    window_.handleEvents(on_close, on_key_pressed, on_key_released);
 }
 
-void Game::Update() {}
+void Game::Update() {
+    sf::Vector2f movement(0.f, 0.f);
+    if (is_move_up_) {
+        movement.y -= 1.0f;
+    }
+    if (is_move_down_) {
+        movement.y += 1.0f;
+    }
+    if (is_move_left_) {
+        movement.x -= 1.0f;
+    }
+    if (is_move_right_) {
+        movement.x += 1.0f;
+    }
+
+    player_.move(movement);
+}
 
 void Game::Render() {
     window_.clear();
     window_.draw(player_);
     window_.display();
+}
+
+void Game::HandlePlayerInput(sf::Keyboard::Scancode key, bool is_pressed) {
+    using enum sf::Keyboard::Scancode;
+    switch (key) {
+    case W: 
+        is_move_up_ = is_pressed;
+        break;
+    case S:
+        is_move_down_ = is_pressed;
+        break;
+    case A:
+        is_move_left_ = is_pressed;
+        break;
+    case D:
+        is_move_right_ = is_pressed;
+        break;
+    default:
+        break;
+    }
 }
